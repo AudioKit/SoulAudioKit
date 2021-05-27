1615a25ac04fb9a01730242c8493edd8bfdc6a16
Aurelius Prochazka <aurephd@gmail.com>
2021-05-27 11:40:02 -0700

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -5,34 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "SoulAudioKit",
-    platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SoulAudioKit",
-            targets: ["SoulAudioKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/AudioKit/AudioKit", .branch("develop")),
-    ],
+    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
+    products: [.library(name: "SoulAudioKit", targets: ["SoulAudioKit"])],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("develop"))],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SoulAudioKit",
-            dependencies: ["AudioKit", "CSoulAudioKit"]),
-        .target(
-            name: "CSoulAudioKit",
-            dependencies: ["AudioKit"],
-            publicHeadersPath: "include",
-            cxxSettings: [ .headerSearchPath(".")]),
-        .testTarget(
-            name: "SoulAudioKitTests",
-            dependencies: ["SoulAudioKit"],
-            resources: [.copy("TestResources/")]),
+        .target(name: "SoulAudioKit", dependencies: ["AudioKit", "CSoulAudioKit"]),
+        .target(name: "CSoulAudioKit", dependencies: ["AudioKit"]),
+        .testTarget(name: "SoulAudioKitTests", dependencies: ["SoulAudioKit"], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: .cxx14
 )
+    
