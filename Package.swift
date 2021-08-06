2678bd3449005223ab9f98e72d154e44d365dd78
Aurelius Prochazka <aurephd@gmail.com>
2021-08-06 16:31:25 -0700

    Use AudioKit 5.2.0+

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SoulAudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "SoulAudioKit", targets: ["SoulAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("develop"))],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
     targets: [
         .target(name: "SoulAudioKit", dependencies: ["AudioKit", "CSoulAudioKit"]),
         .target(name: "CSoulAudioKit", dependencies: ["AudioKit"]),
