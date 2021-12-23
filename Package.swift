e0334228042814216ff12b81499f911632440104
Aurelius Prochazka <aurephd@gmail.com>
2021-12-22 16:57:14 -0800

    Updated to AK 5.3

--- Package.swift
+++ Package.swift
@@ -7,10 +7,13 @@ let package = Package(
     name: "SoulAudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "SoulAudioKit", targets: ["SoulAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
+    dependencies: [
+        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0"),
+        .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.3.0"),
+    ],
     targets: [
-        .target(name: "SoulAudioKit", dependencies: ["AudioKit", "CSoulAudioKit"]),
-        .target(name: "CSoulAudioKit", dependencies: ["AudioKit"]),
+        .target(name: "SoulAudioKit", dependencies: ["AudioKit", "AudioKitEX", "CSoulAudioKit"]),
+        .target(name: "CSoulAudioKit", dependencies: ["AudioKit", "AudioKitEX"]),
         .testTarget(name: "SoulAudioKitTests", dependencies: ["SoulAudioKit"], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: .cxx14
